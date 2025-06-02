283bcff1194154819e2e93c63c7cfcc3fafba5bb
palnativenkatesh <166120319+palnativenkatesh@users.noreply.github.com>
2025-06-02 11:34:36 +0530

    app.js

---
+++ FrontEnd/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Resume Parser</h2>
+      <ResumeParser />
+    </div>
+  );
+}
+
+export default App;
